99705496f846425608ba3361d3df525294ad4372
dminakova <dminakova@cinimex.ru>
2021-11-01 16:10:16 +0300

    ДЗ по циклам

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,28 @@
+public class Cycle {
+    public static void main(String[] args) {
+        System.out.println("Выводим на консоль с помощью цикла for все числа от [0, 20]");
+        for (int i = 0; i < 21; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Выводим на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)");
+        int i = 6;
+        while (i > -7) {
+            System.out.println(i);
+            i-=2;
+        }
+
+        System.out.println("Выводим на консоль сумму нечетных чисел от [10, 20] подсчитанную с помощью цикла do-while");
+        i = 10;
+        int result = 0;
+        do {
+            if (i%2 != 0) {
+                result +=i;
+                System.out.print(i + " + ");
+            }
+            i++;
+        }
+        while (i <= 20);
+        System.out.print("0 =" + result);
+    }
+}
