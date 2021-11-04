1aa4d20aee46f613f959f24f24240e3499be44a5
dminakova <dminakova@cinimex.ru>
2021-11-04 18:39:46 +0300

    ДЗ по выводу Unicode в цикле

---
+++ Lesson_1/CycleUnicode.java
@@ -0,0 +1,7 @@
+public class CycleUnicode{
+    public static void main(String[] args) {
+            for (int i = 33; i <= 126; i++) {
+                System.out.println((char) i);
+            }
+    }
+}
