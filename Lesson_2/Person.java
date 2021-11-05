f05282c0d9804897ea641db66e61e310c9c77bb8
dminakova <dminakova@cinimex.ru>
2021-11-05 15:04:50 +0300

    ДЗ 2 занятие шаг 2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    char gender = 'Ж';
+    String name = "Дарья";
+    float height = 170f;
+    float weight = 57.5f;
+    int age = 30;
+
+    void go() {
+        System.out.println("Идти");
+    }
+
+    void seat() {
+        System.out.println("Сидеть");
+    }
+
+    void run() {
+        System.out.println("Бежать");
+    }
+
+    void talk() {
+        System.out.println("Говорить");
+    }
+
+    void trainJava() {
+        System.out.println("Учить Java");
+    }
+}
