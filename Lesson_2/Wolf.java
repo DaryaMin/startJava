27745ec4fa62fc8c89e36f1ad14a873f121ee610
dminakova <dminakova@cinimex.ru>
2021-11-05 18:16:12 +0300

    ДЗ занятие 2 п.3

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+
+    char gender = 'м';
+    String nick = "Дружок";
+    int age = 3;
+    String color = "серый";
+
+    void go() {
+        System.out.println("Идёт");
+    }
+
+    void seat() {
+        System.out.println("Сидит");
+    }
+
+    void run() {
+        System.out.println("Бежит");
+    }
+
+    void howl() {
+        System.out.println("Воет");
+    }
+
+    void hunt() {
+        System.out.println("Охотится");
+    }
+}
