a406a3552dbaab25a8448f9da518aa9d1f236201
dminakova <dminakova@cinimex.ru>
2021-11-16 18:18:59 +0300

    Удаление лишней строки

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -46,7 +46,6 @@ public class GuessNumber {
                 System.out.println(playerTwo.getName() + " победил");
                 break;
             }
-
         } while (!isPlayerOneGuess && !isPlayerTwoGuess);
     }
 
